46b2fb21238a8fb871b34e229878d529a03d851c
Yao Qian <yaoqianlow89@gmail.com>
2023-07-01 16:48:02 +0800

    main.cpp

--- Main.cpp
+++ Main.cpp
@@ -2,6 +2,8 @@
  * FILENAME: Main.cpp
  */
 
+#include<iostream>
+
 #include "Asia_Pacific_Home.h"
 #include "DoublyCircularLinkedList.h"
 
@@ -9,28 +11,27 @@ int main()
 {
     // Asia_Pacific_Home APH;
     // APH.homePage();
-    // return 0;
 
     /** DEMO - Tutorial7 - LinkedLIst-Doubly */
 
-    // int n = 0;
-    // std::cout << "Enter number of student name to store = ";
-    // std::cin >> n;
-    // DoublyCircularLinkedList<string> nameLst;
+    int n = 0;
+    std::cout << "Enter number of student name to store = ";
+    std::cin >> n;
+    DoublyCircularLinkedList<string> nameLst;
 
-    // for (int i = 0; i < n; i++)
-    // {
-    //     string tmp;
-    //     std::cout << "\n>Enter name " << (i + 1) << " = ";
-    //     std::cin >> tmp;
-    //     nameLst.insert(tmp);
-    // }
+    for (int i = 0; i < n; i++)
+    {
+        string tmp;
+        std::cout << "\n>Enter name " << (i + 1) << " = ";
+        std::cin >> tmp;
+        nameLst.insert(tmp);
+    }
 
-    // nameLst.display();
-    // std::cout << endl;
+    nameLst.display();
+    std::cout << endl;
 
-    // std::cout << "\nProgram ended!" << endl;
-    // std::cout << endl;
+    std::cout << "\nProgram ended!" << endl;
+    std::cout << endl;
 
     return 0;
 }
